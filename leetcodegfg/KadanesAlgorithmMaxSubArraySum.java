5af4fd3f3722bd0b03294c75ca9bc2ccdab5f71e
Rohan Kodipaka <rohankodipaka777@gmail.com>
2024-07-19 22:23:01 +0530

    feat : add comments to KadanesAlgorithmMaxSubArraySum.java file

--- leetcodegfg/KadanesAlgorithmMaxSubArraySum.java
+++ leetcodegfg/KadanesAlgorithmMaxSubArraySum.java
@@ -3,7 +3,7 @@ package com.leetcodegfg;
 public class KadanesAlgorithmMaxSubArraySum {
     public static void main(String[] args) {
         int[] numbers = {-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println(findMaxSubArraySum(numbers));
+        System.out.println(findMaxSubArraySum(numbers)); //optimal approach --> O(N)
     }
 
     public static long findMaxSubArraySum(int[] numbers){
@@ -16,6 +16,7 @@ public class KadanesAlgorithmMaxSubArraySum {
             if(sum>maxSum){
                 maxSum = sum;
             }
+            // If sum < 0 --> discard the sum calculated
             if(sum<0){
                 sum = 0;
             }
